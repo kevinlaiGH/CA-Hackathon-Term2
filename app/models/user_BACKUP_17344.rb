d5772d7f9c2ac213c8e2e936d2e29b58392d3aa9
YOUR NAME <YOUR@EMAIL.com>
2017-10-20 14:03:14 +1100

    testing errors

---
+++ app/models/user_BACKUP_17344.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin], multiple: false)                                      ##
+  ############################################################################################
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+<<<<<<< HEAD
+  has_many :charges
+=======
+  has_many :reviews
+
+>>>>>>> 67642707f6c64398cd945f06a9173facac9f6480
+end
